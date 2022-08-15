d372b6f5def8774e40225ab0677de8b462d8daa7
Dmitry <dima_gazavto@mail.ru>
2022-08-15 22:15:52 +0300

    Task004

---
+++ Task004/Program.cs
@@ -0,0 +1,11 @@
+﻿/* Написать программу, которая принимает на вход число (N) и выдает таблицу квадратов от 1 до N
+5 -> 1, 4, 9, 16, 25
+2 -> 1, 4*/
+
+Console.Write("Введите любое число для последовательного вывода квадратов ");
+int number = int.Parse(Console.ReadLine());
+
+for(int i = 1; i <= number; i++){
+    int result = Convert.ToInt32(Math.Pow(i, 2));
+    Console.WriteLine(result);
+}
