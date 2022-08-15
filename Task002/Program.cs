622f20512fb3ae8b5e03be19fcba071b7ce9da62
Dmitry <dima_gazavto@mail.ru>
2022-08-15 20:42:30 +0300

    Task002

---
+++ Task002/Program.cs
@@ -0,0 +1,16 @@
+﻿//Напишите программу, которая по заданному номеру четверти показывает диапазон возможных координат точек в этой четверти (х и y)
+
+Console.Write("Введите номер четверти от 1 до 4:  ");
+int number = int.Parse(Console.ReadLine());
+
+if(number == 1){
+    Console.WriteLine("Диапазоны по оси x > 0, y > 0");
+} else if(number == 2){
+    Console.WriteLine("Диапазоны по оси x < 0, y > 0");
+} else if(number == 3){
+    Console.WriteLine("Диапазоны по оси x < 0, y < 0");
+} else if(number == 4){
+    Console.WriteLine("Диапазоны по оси x > 0, y < 0");
+} else if(number > 4 || number < 1){
+    Console.WriteLine("Номер четверти введен некорректно");
+}
