0e3c798e07c278278d3784c467edbd36793a9059
Dmitry <dima_gazavto@mail.ru>
2022-08-15 21:08:11 +0300

    Task002_v2

--- Task002/Program.cs
+++ Task002/Program.cs
@@ -3,7 +3,7 @@
 Console.Write("Введите номер четверти от 1 до 4:  ");
 int number = int.Parse(Console.ReadLine());
 
-if(number == 1){
+/*if(number == 1){
     Console.WriteLine("Диапазоны по оси x > 0, y > 0");
 } else if(number == 2){
     Console.WriteLine("Диапазоны по оси x < 0, y > 0");
@@ -13,4 +13,34 @@ if(number == 1){
     Console.WriteLine("Диапазоны по оси x > 0, y < 0");
 } else if(number > 4 || number < 1){
     Console.WriteLine("Номер четверти введен некорректно");
+}*/
+
+//Альтернативное решение данной задачи с помощью switch
+
+switch(number){
+    case 1:
+    {
+        Console.WriteLine("Диапазоны по оси x > 0, y > 0");
+        break;
+    }
+    case 2:
+    {
+        Console.WriteLine("Диапазоны по оси x < 0, y > 0");
+        break;
+    }
+    case 3:
+    {
+        Console.WriteLine("Диапазоны по оси x < 0, y < 0");
+        break;
+    }
+    case 4:
+    {
+        Console.WriteLine("Диапазоны по оси x > 0, y < 0");
+        break;
+    }
+    default:
+    {
+        Console.WriteLine("Номер четверти введен некорректно");
+        break;
+    }
 }
