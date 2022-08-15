c87241cd1b5b972145f6d1d1cc4e5a961d2cbfea
Dmitry <dima_gazavto@mail.ru>
2022-08-15 20:22:03 +0300

    Task0001

--- Task001/Program.cs
+++ Task001/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Введите число по оси х: ");
+int x = int.Parse(Console.ReadLine());
+Console.Write("Введите число по оси y: ");
+int y = int.Parse(Console.ReadLine());
+
+if(x > 0 && y > 0){
+    Console.Write("Это первая четверть");
+} else if(x < 0 && y > 0) {
+    Console.Write("Это вторая четверть");
+} else if(x < 0 && y < 0) {
+    Console.Write("Это третья четверть");
+} else if(x > 0 && y < 0) {
+    Console.Write("Это четвертая четверть");
+} 
